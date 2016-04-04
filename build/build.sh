b20abbd72859305bcb32929960059e7c117becc1
Julius <julius@cloud.house>
2016-04-04 04:05:43 +0000

    Move libs to build folder

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # compile java files
-mkdir -p ../WebContent/WEB-INF/classes
-javac -cp "../lib/*" -d ../WebContent/WEB-INF/classes ../src/*
+mkdir -p WebContent/WEB-INF/classes
+javac -cp "build/lib/*" -d WebContent/WEB-INF/classes ../src/*
 
-mkdir -p ../WebContent/WEB-INF/lib
-cp -R "../lib" "../WebContent/WEB-INF/"
+mkdir -p WebContent/WEB-INF/lib
+cp -R "build/lib" "WebContent/WEB-INF/"
 
-cd ../WebContent
-jar -cvf ../app.war *
+# create war file
+mkdir -p deploy
+cd WebContent
+jar -cvf ../deploy/app.war *
